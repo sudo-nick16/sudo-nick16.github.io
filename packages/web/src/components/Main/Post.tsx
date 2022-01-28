e582bc81a0b0154e4e00826a6e815f78773551e2
Nikit <singhnikit16@gmail.com>
2022-01-28 13:18:59 +0530

    fix: fixed post comp styling

--- packages/web/src/components/Main/Post.tsx
+++ packages/web/src/components/Main/Post.tsx
@@ -17,17 +17,17 @@ const Post: NextPage<PostProps> = ({ post, className }) => {
       key={post.id}
       className={`flex ${
         description ? "flex-col" : "items-center"
-      } cursor-pointer hover:scale-[1.02] shadow-black shadow-lg tn mx-auto rounded-lg my-2 w-1/2 sm:max-w-4/6 sm:w-52 ${className}`}
+      } cursor-pointer hover:scale-[1.02] shadow-black shadow-lg tn mx-auto rounded-lg my-2 w-[55%] sm:max-w-4/6 sm:w-52 ${className}`}
     >
       {description ? (
         <img
           src={description}
           alt={title}
-          className={`rounded-t-lg w-full h-28 object-cover`}
+          className={`rounded-t-lg w-full h-32 object-cover`}
         />
       ) : null}
       <h1
-        className={`font-light w-full font-poppin text-sm cursor-pointer tn my-4 sm:my-3 text-center px-2 sm:px-2 truncate-2`}
+        className={`font-light w-full font-poppin text-sm cursor-pointer tn my-3 sm:my-auto sm:py-3 text-center px-2 sm:px-2 truncate-2`}
       >
         {title}
       </h1>
